6ebf325f47efc55ea338b3aa81f6e6c4f3f9d026
Grzegorz Pach <grzegorz.andrzej.pach@gmail.com>
2024-08-13 13:57:11 +0200

    Add camelcase option

--- anonymize.ts
+++ anonymize.ts
@@ -1,78 +1,76 @@
 #!/usr/bin/env node
 
-import * as fs from "fs";
-import * as path from "node:path";
-import * as crypto from "crypto";
-import { Command } from "commander";
-import { faker } from "@faker-js/faker";
+import * as fs from 'fs';
+import * as crypto from 'crypto';
+import { Command } from 'commander';
+import { faker } from '@faker-js/faker';
+import camelCase from 'camelcase';
 
 const program = new Command();
 
 program
-  .option("-f, --file <path>", "JSON file to anonymize")
-  .parse(process.argv);
+    .option('-f, --file <path>', 'JSON file to anonymize')
+    .option('-c, --camelcase', 'Convert object keys from snake_case to camelCase')
+    .parse(process.argv);
 
 const options = program.opts();
 
 if (!options.file) {
-  console.error("Please provide a JSON file to anonymize with -f option");
-  process.exit(1);
+    console.error('Please provide a JSON file to anonymize with -f option');
+    process.exit(1);
 }
 
 const hashUuid = (uuid: string): string => {
-  return crypto.createHash("sha256").update(uuid).digest("hex");
+    return crypto.createHash('sha256').update(uuid).digest('hex');
 };
 
 const getFakeString = (): string => {
-  return faker.lorem.word();
+    return faker.lorem.word();
 };
 
 const anonymizeObject = (obj: any): any => {
-  if (typeof obj === "string") {
-    // Simple check for UUID (pattern: 'xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx')
-    if (
-      /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/.test(
-        obj,
-      )
-    ) {
-      return hashUuid(obj);
-    } else {
-      return getFakeString();
+    if (typeof obj === 'string') {
+        // Simple check for UUID (pattern: 'xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx')
+        if (/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/.test(obj)) {
+            return hashUuid(obj);
+        } else {
+            return getFakeString();
+        }
     }
-  }
 
-  if (Array.isArray(obj)) {
-    return obj.map(anonymizeObject);
-  }
-
-  if (typeof obj === "object" && obj !== null) {
-    const result: Record<string, any> = {};
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        result[key] = anonymizeObject(obj[key]);
-      }
+    if (Array.isArray(obj)) {
+        return obj.map(anonymizeObject);
     }
-    return result;
-  }
 
-  return obj;
+    if (typeof obj === 'object' && obj !== null) {
+        const result: { [key: string]: any } = {};
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                const updatedKey = options.camelcase ? camelCase(key) : key;
+                result[updatedKey] = anonymizeObject(obj[key]);
+            }
+        }
+        return result;
+    }
+
+    return obj;
 };
 
-fs.readFile(options.file, "utf8", (err, data) => {
-  if (err) {
-    console.error(`Error reading file: ${err.message}`);
-    process.exit(1);
-  }
-
-  let json: any = JSON.parse(data);
-  json = anonymizeObject(json);
-
-  const outputFilename = `anonymized_${options.file}`;
-  fs.writeFile(outputFilename, JSON.stringify(json, null, 2), (err) => {
+fs.readFile(options.file, 'utf8', (err, data) => {
     if (err) {
-      console.error(`Error writing file: ${err.message}`);
-      process.exit(1);
+        console.error(`Error reading file: ${err.message}`);
+        process.exit(1);
     }
-    console.log(`Anonymized JSON saved to ${outputFilename}`);
-  });
+
+    let json: any = JSON.parse(data);
+    json = anonymizeObject(json);
+
+    const outputFilename = `anonymized_${options.file}`;
+    fs.writeFile(outputFilename, JSON.stringify(json, null, 2), (err) => {
+        if (err) {
+            console.error(`Error writing file: ${err.message}`);
+            process.exit(1);
+        }
+        console.log(`Anonymized JSON saved to ${outputFilename}`);
+    });
 });
