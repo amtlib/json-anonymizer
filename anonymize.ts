c208c51a5788bbf295adc9a6d1c7adb3e7d9c8c9
Grzegorz Pach <grzegorz.andrzej.pach@gmail.com>
2024-08-13 15:31:19 +0200

    Fix date matcher regex

--- anonymize.ts
+++ anonymize.ts
@@ -31,7 +31,7 @@ const snakeToCamel = (str: string): string => {
 };
 
 const isIsoDateString = (str: string): boolean => {
-  const isoDateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+  const isoDateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/;
   return isoDateRegex.test(str);
 };
 
